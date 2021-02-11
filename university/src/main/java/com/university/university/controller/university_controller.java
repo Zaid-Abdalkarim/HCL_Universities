ae327c829e2f7b03e341abac6fdf373c7791826d
Zaid Abdalkarim <zaidabdalkarim@gmail.com>
2021-02-10 16:31:38 -0800

    second commit

--- university/src/main/java/com/university/university/controller/university_controller.java
+++ university/src/main/java/com/university/university/controller/university_controller.java
@@ -6,11 +6,12 @@ import com.university.university.model.UniversityModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
+@RestController
 public class university_controller {
 
     @Autowired
